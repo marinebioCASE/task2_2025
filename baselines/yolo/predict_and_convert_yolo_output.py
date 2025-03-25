0fca24348379442c9aa7d5730460550c89177fc3
Clea Parcerisas <clea.parcerisas@vliz.be>
2025-03-25 13:07:09 +0100

    small adjustments in post-processing the data of yolo predictions

--- baselines/yolo/predict_and_convert_yolo_output.py
+++ baselines/yolo/predict_and_convert_yolo_output.py
@@ -1,12 +1,15 @@
 import json
 from ultralytics import YOLO
+import yaml
 
 import preprocess_data
 
 
-def predict(ds, model, conf=0.1):
+def predict(ds, conf=0.1):
     predictions_folder = ds.path_to_dataset.joinpath('predictions')
     if not predictions_folder.exists():
+        model_path = input('Folder still not predicted. Where is the path to the model to predict?')
+        model = YOLO(model_path)
         if ds.images_folder.exists():
             results_list = model(source=str(ds.images_folder),
                                  project=str(ds.path_to_dataset),
@@ -25,13 +28,14 @@ def predict(ds, model, conf=0.1):
 
 if __name__ == '__main__':
     path_to_dataset = input('Where is the dataset folder?')
-    model_path = input('Where is the model path?')
 
     config_path = './dataset_config.json'
     f = open(config_path)
     config = json.load(f)
 
+    with open('./custom_joined.yaml', 'r') as file:
+        yolo_config = yaml.safe_load(file)
+
     yolo_ds = preprocess_data.YOLODataset(config, path_to_dataset)
-    yolo_model = YOLO(model_path)
-    output_folder = predict(yolo_ds, yolo_model)
-    yolo_ds.convert_yolo_detections_to_csv(output_folder, class_encoding=config['class_encoding'])
+    output_folder = predict(yolo_ds)
+    yolo_ds.convert_yolo_detections_to_csv(output_folder, reverse_class_encoding=yolo_config['names'])
